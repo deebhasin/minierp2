26a3fb480f33300264c4d8f58844df4a499296e4
kunwaranand <kunwaranand74@gmail.com>
2022-01-04 11:36:17 +0530

    test

--- lib/screens/formscreen.dart
+++ lib/screens/formscreen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
 class FormScreen extends StatelessWidget {
-  const FormScreen({Key? key}) : super(key: key);
-
   FormScreen();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
